6fd5cd776ea54da4b3f8572b259eb63d400654e8
thanhnguyen <ntthanhhanam@gmail.com>
2026-04-04 12:26:10 +0700

    up memory-service

---
+++ BE/memory-service/src/main/java/com/java/memoryservice/MemoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.java.memoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryServiceApplication.class, args);
+    }
+
+}
